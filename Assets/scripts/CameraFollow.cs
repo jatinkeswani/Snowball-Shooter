b4cbd083362e5b76087e3bc9f95d99669f5939f5
Jatin Keswani <jatinkeswani151@gmail.com>
2021-02-28 14:28:00 +0530

    actual game scene 1

--- Assets/scripts/CameraFollow.cs
+++ Assets/scripts/CameraFollow.cs
@@ -12,6 +12,8 @@ public class CameraFollow : MonoBehaviour
     void LateUpdate()
     {
         Vector3 desiredPosition = target.position + offset;
+        // desiredPosition.x = Mathf.Clamp(desiredPosition.x, -1f, 1f);
+        desiredPosition.x /= 2f;
         Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothPosition;
         // transform.LookAt(target);
